66a0a29f2a7c4fe000914aabb2d16da17a4c25aa
Dinithi1625403 <dinithi1625403@gmail.com>
2025-11-07 23:46:38 +0530

    added routes to index js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
+import rentalRoutes from "./routes/rentalRotes.js";
 dotenv.config();
 const app = express();
 
@@ -27,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/rentals", rentalRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
